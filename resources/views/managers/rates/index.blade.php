c2210e307a1355344af0b84b087754a66d46cf66
Kevin <fsn2uu@gmail.com>
2017-12-12 18:33:35 -0600

    general housekeeping

--- resources/views/managers/rates/index.blade.php
+++ resources/views/managers/rates/index.blade.php
@@ -19,16 +19,61 @@
             @if($schedules->count() < 1)
                 <h3>You have no rate schedules!</h3>
             @else
-                <table class="table is-hoverable">
-                    @foreach($schedules as $r)
-                        <tr>
-                            <td>
-                                <a href="{{ route('managers.rates.show', $r->id) }}">{{ $r->name }}</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                </table>
+                <?php $schedule_count = $schedules->count(); $i=1;?>
+
+                <div class="columns">
+                    <div class="column">
+                        @foreach ($schedules as $schedule)
+                            <div class="card m-b-20">
+                                <header class="card-header">
+                                    <p class="card-header-title">
+                                        {{ $schedule->name }}
+                                    </p>
+                                </header>
+                                <div class="card-content">
+                                    <div class="content">
+                                        <table class="table">
+                                            <thead>
+                                                <tr>
+                                                    <th></th>
+                                                    <th>Start Date</th>
+                                                    <th>End Date</th>
+                                                    <th>Daily Rate</th>
+                                                    <th>Weekly Rate</th>
+                                                </tr>
+                                            </thead>
+                                            @foreach ($schedule->rates as $r)
+                                                <tr>
+                                                    <td>{{ $r->name }}</td>
+                                                    <td>{{ $r->start }}</td>
+                                                    <td>{{ $r->end }}</td>
+                                                    <td>${{ number_format($r->daily, 2) }}</td>
+                                                    <td>${{ number_format($r->weekly, 2) }}</td>
+                                                </tr>
+                                            @endforeach
+                                        </table>
+                                    </div>
+                                </div>
+                                <footer class="card-footer">
+                                    <a href="{{ route('managers.rates.show', $schedule->id) }}" class="card-footer-item">Edit</a>
+                                </footer>
+                            </div>
+                            <?php
+                            if($i % 3 == 0)
+                            {
+                                echo '</div><div class="columns">';
+                            }
+
+                            $i++;
+                            ?>
+                        @endforeach
+                    </div>
+                </div>
             @endif
+
+
+
+
         </div>
     </div>
 
